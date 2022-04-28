1917566e1e9afc429b91e551d76343fae89fc0ac
Gutemberg Medeiros <gmedeiros@gmail.com>
2022-04-28 15:28:05 -0300

    Fixing AWS_SECURITY_GROUP.VPC_ID

--- main.tf
+++ main.tf
@@ -3,9 +3,26 @@ provider "aws" {
   // version = "~> 4.11.0"
 }
 
-## NETWORK
-## VPC, SUBNETs, and IPs
-resource "aws_vpc" "web-vpc" {
+## INSTANCE
+## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "web" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  key_name      = "tf-aws"
+
+  security_groups = [aws_security_group.web-sg.name]
+
+  tags = {
+    WebServer = "WebServer"
+  }
+}
+
+output "instance_public_dns" {
+  value = "aws_instance.web.public_dns"
+}
+
+## VPC
+resource "aws_vpc" "web_vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
@@ -13,31 +30,12 @@ resource "aws_vpc" "web-vpc" {
   }
 }
 
-resource "aws_subnet" "web-subnet" {
-  vpc_id            = aws_vpc.web-vpc
-  cidr_block        = "172.16.0.0/24"
-  availability_zone = "us-east-1"
-
-  tags = {
-    name = "WebServer"
-  }
-
-}
-
-resource "aws_network_interface" "foo" {
-  subnet_id  = aws_instance.web
-  private_ip = "172.16.0.1"
-
-  tags = {
-    name = "Primary_network_interface"
-  }
-}
-
 ## SECURITY GROUPS
 ## 
-resource "aws_security_group" "web" {
+resource "aws_security_group" "web-sg" {
   name        = "web-security-group"
   description = "Allow access to web server"
+  vpc_id = "aws_vpc.web_vpc"
 
   ingress {
     description      = "Allow SSH"
@@ -52,20 +50,26 @@ resource "aws_security_group" "web" {
   }
 }
 
-## INSTANCE
-## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
-resource "aws_instance" "web" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
-  key_name      = "tf-aws"
-
-  security_groups = [aws_security_group.web.name]
+## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+resource "aws_subnet" "web_subnet" {
+  vpc_id            = "aws_vpc.web_vpc"
+  cidr_block        = "172.16.0.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    WebServer = "WebServer"
+    "name" = "Main"
   }
 }
 
-output "instance_public_dns" {
-  value = "aws_instance.web.public_dns"
+
+resource "aws_network_interface" "foo" {
+  subnet_id  = "aws_subnet.web_subnet"
+  private_ip = "172.16.0.1"
+
+  tags = {
+    name = "Primary_network_interface"
+  }
 }
+
+
+
